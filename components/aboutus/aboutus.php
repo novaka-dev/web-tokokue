76cc3cc8c03bbd9b67c0279b94668ba8e6b453e2
aryakimi <aryaluqhakim26@gmail.com>
2026-05-18 21:16:10 +0700

    create header and about page

---
+++ components/aboutus/aboutus.php
@@ -0,0 +1,44 @@
+<!-- aboutus.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>About Us</title>
+
+  <link rel="stylesheet" href="aboutus.css">
+</head>
+
+<body>
+
+  <section class="about">
+
+    <!-- IMAGE -->
+    <div class="about-image">
+      <img src="assets/img/tokokue.jpeg" alt="">      
+    </div>
+
+    <!-- TEXT -->
+    <div class="about-text">
+
+      <h1>Tentang Kami</h1>
+
+      <div class="line"></div>
+
+      <p>
+        Ann's Bakehouse & Creamery was established in 2014.
+        We prioritize service excellence and the needs of our
+        customers by building the most effective cake delivery
+        team and customer service team to achieve our goal
+        of becoming the most reliable one-click-away-service
+        online cake shop in Jakarta.
+      </p>
+
+    </div>
+
+  </section>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>Document</title>
+  
+  <link rel="stylesheet" href="assets/styles/main.css">
+
 </head>
+
 <body>
-	<?php include "navbar.php"; ?>
+
+  <?php
+    include 'components/header/header.php';
+  ?>
+
+  <?php
+    include 'components/aboutus/aboutus.php';
+  ?>
+
 </body>
 </html>
