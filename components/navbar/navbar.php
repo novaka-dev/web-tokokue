e332c6010fcf8ba05cf540836119d86da312d1eb
Restu21aelah <restuajiajah@gmail.com>
2026-05-31 18:31:27 +0700

    resolve merge conflict

--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -1,1 +1,25 @@
- 
+<nav>
+	<button class="burger" type="button" command="open-popover" popovertarget="navbar-dialog">
+	  <div class="bar1"></div>
+	  <div class="bar2"></div>
+	  <div class="bar3"></div>
+	</button>
+	<div class="navbar-dialog" popover id="navbar-dialog">
+		<aside class="navbar-sidebar" id="navbar-sidebar" >
+			<button class="burger" type="button" command="close-popover" popovertarget="navbar-dialog" style="position: absolute; right: 0;">
+	  			<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#AB9164" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+			</button>
+		</aside>
+	</div>
+	
+	<img class="brand-logo" src="../../assets/image/anns.png" alt="anns">
+	<div class="menu-right">
+		<a class="sign" href="#">Sign in</a>
+		<button type="button" class="search-button">	
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#AB9164" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>		
+		</button>
+		<a href="#" class="navbar-cart">
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#AB9164" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
+		</a>
+	</div>
+</nav>
--- index.php
+++ index.php
@@ -3,38 +3,29 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 	<title>Document</title>
-
-  <link rel="stylesheet" href="assets/styles/main.css">
-
+	<link rel="stylesheet" href="assets/styles/main.css">
 </head>
 
 <body>
-
   <?php
-    include 'components/header/header.php';
+  include "components/navbar/navbar.php";
+  include 'components/header/header.php';
   ?>
+	<div class="content">
+	  <div class="container">
+	  <?php
+	    require_once 'components/product-card/product-card.php';
+	    require_once 'data/products.php';
 
-<div class="content">
-  <div class="container">
-  <?php
-    require_once 'components/product-card/product-card.php';
-    require_once 'data/products.php';
-
-    render_product_grid($products);
-  ?>
-</div>
-
-</div>
-
+	    render_product_grid($products);
+	  ?>
+	  </div>
+	</div>
   <div class="container">
   <?php
     include 'components/aboutus/aboutus.php';
   ?>
   </div>
-
-
-
 </body>
 </html>
