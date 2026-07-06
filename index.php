734221250e77ff7ff60760e21edf0bcfb3271dd7
aryakimi <aryaluqhakim26@gmail.com>
2026-07-06 21:42:06 +0700

    perubahan aruakun

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Ann's Bakery KUUUUU</title>
 	<link rel="stylesheet" href="assets/styles/main.css?v=2">
 </head>
 
